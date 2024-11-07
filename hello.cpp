b36423960b475db5cadb0643f58c5e2a757a9f5c
sohun-cs <asaduzzamansohun5@gmail.com>
2024-11-07 22:50:57 +0600

    void data type

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
+
+void fun(){
+    cout << "This is void data type";
+    return;
+}
 int main(){
-    cout << "Hello World in C++ ";
-    cout << " Let's start something new. ❤";
+    cout << "Hello World in C++ " <<endl<< "Let's start something new. ❤" << endl;
+    fun();
     return 0;
 }
+
