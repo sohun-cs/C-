2dbe1dc15ac9330f33e44bc602143c4e3a4b9633
sohun-cs <asaduzzamansohun5@gmail.com>
2024-11-10 17:13:50 +0600

    Operators: Logical Operators

--- operators.cpp
+++ operators.cpp
@@ -6,19 +6,29 @@ int main(){
     int num2 = 8;
 
     // Arithmetic Operators
-    cout <<num1+num2<< endl;
-    cout <<num1-num2<< endl;
-    cout <<num1/num2<< endl;
-    cout <<num1*num2<< endl;
-    cout <<num1%num2<< endl;
+    //cout <<num1+num2<< endl;
+    //cout <<num1-num2<< endl;
+    //cout <<num1/num2<< endl;
+    //cout <<num1*num2<< endl;
+    //cout <<num1%num2<< endl;
 
     // Relational Operators
-    cout <<(num1<num2)<< endl; //false
-    cout <<(num1>num2)<< endl; //true
-    cout <<(num1!=num2)<< endl; //true
-    cout <<(num1==num2)<< endl; //false
-    cout <<(num1<=num2)<< endl; //false
-    cout <<(num1>=num2)<< endl; //true
+    //cout <<(num1<num2)<< endl; //false
+    //cout <<(num1>num2)<< endl; //true
+    //cout <<(num1!=num2)<< endl; //true
+    //cout <<(num1==num2)<< endl; //false
+    //cout <<(num1<=num2)<< endl; //false
+    //cout <<(num1>=num2)<< endl; //true
+
+    //Logical Operators
+    bool a = true;
+    bool b = false;
+
+    cout <<( a&&b )<< endl; //false
+    cout <<( a||b )<< endl; //true
+    cout <<( !a )<< endl; //false
+    cout <<( !b )<< endl; //true
+    
 
     return 0;
 }
